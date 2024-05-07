97c4e0fc6f8d934dc8649a04ca406addc034f2bd
kevin <128761880+Kevinaaaaqw@users.noreply.github.com>
2024-05-07 21:17:18 +0800

    113/05/07 菜單完成

---
+++ toDoList/src/components/MenuView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+import { ref, computed, unref, type Ref } from 'vue'
+
+
+const route = useRoute()//路由
+const path = computed(() => route.path)//網址
+const router = useRouter()//使用路由
+
+//當前頁面CSS
+const checkPath = (path: string | Ref, target: string) => {
+    if (unref(path) === target) return 'text-bg-4 menu-line text-center'
+    return ''
+}
+
+//移動路由
+const toPage = (path: string) => {
+    router.push(path)
+}
+
+</script>
+<template>
+    <div class="w-full bg-base h-80px flex justify-center">
+        <div
+            class="max-w-1000px w-full flex justify-around h-full children:flex children:relative children:items-center children:h-full children:ps-5 children:pe-5 children:title hover:children:cursor-pointer ">
+            <div :class="checkPath(path, '/MyTasks')" @click="() => { toPage('/MyTasks') }">My Tasks</div>
+            <div :class="checkPath(path, '/InProgress')" @click="() => { toPage('/InProgress') }">In Progress </div>
+            <div :class="checkPath(path, '/Completed')" @click="() => { toPage('/Completed') }">Completed</div>
+        </div>
+    </div>
+</template>
+<style>
+.menu-line::after {
+    content: "";
+    display: block;
+    background-color: #00408B;
+    position: absolute;
+    width: 100%;
+    height: 5px;
+    bottom: 0px;
+    left: 0px;
+}
+</style>
