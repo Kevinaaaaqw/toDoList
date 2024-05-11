af6e09217a439ef51fe831adc62653123051ba06
kevin <128761880+Kevinaaaaqw@users.noreply.github.com>
2024-05-11 12:54:47 +0800

    新增 datePicker 與圖片

--- toDoList/src/main.ts
+++ toDoList/src/main.ts
@@ -1,11 +1,17 @@
 import 'uno.css'
+import '@fortawesome/fontawesome-free'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+//使用日期選擇器
+app.component('VueDatePicker', VueDatePicker);
 
 app.use(createPinia())
 app.use(router)
